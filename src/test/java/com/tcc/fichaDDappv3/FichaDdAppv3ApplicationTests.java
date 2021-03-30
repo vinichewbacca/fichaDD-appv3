27a6667bfef45cd8b779624e17f9625f71859330
vinichewbacca <vinicius.souza@estudante.ifms.edu.br>
2021-03-29 20:17:52 -0400

    Inicio da versao 3 da api da ficha de rpg de mesa Feito por Vinícius A. de Souza

---
+++ src/test/java/com/tcc/fichaDDappv3/FichaDdAppv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.fichaDDappv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaDdAppv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
